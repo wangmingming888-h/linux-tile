c7312039cb142e99a7b10c0860bd42f5998e674d
Chris Metcalf <cmetcalf@mellanox.com>
2017-10-24 17:04:15 -0400

    task_isolation: userspace hard isolation from kernel
    The existing nohz_full mode is designed as a "soft" isolation mode
    that makes tradeoffs to minimize userspace interruptions while
    still attempting to avoid overheads in the kernel entry/exit path,
    to provide 100% kernel semantics, etc.
    
    However, some applications require a "hard" commitment from the
    kernel to avoid interruptions, in particular userspace device driver
    style applications, such as high-speed networking code.
    
    This change introduces a framework to allow applications
    to elect to have the "hard" semantics as needed, specifying
    prctl(PR_TASK_ISOLATION, PR_TASK_ISOLATION_ENABLE) to do so.
    
    The kernel must be built with the new TASK_ISOLATION Kconfig flag
    to enable this mode, and the kernel booted with an appropriate
    "nohz_full=CPULIST isolcpus=CPULIST" boot argument to enable
    nohz_full and isolcpus.  The "task_isolation" state is then indicated
    by setting a new task struct field, task_isolation_flag, to the
    value passed by prctl(), and also setting a TIF_TASK_ISOLATION
    bit in the thread_info flags.  When the kernel is returning to
    userspace from the prctl() call and sees TIF_TASK_ISOLATION set,
    it calls the new task_isolation_start() routine to arrange for
    the task to avoid being interrupted in the future.
    
    With interrupts disabled, task_isolation_start() ensures that kernel
    subsystems that might cause a future interrupt are quiesced.  If it
    doesn't succeed, it adjusts the syscall return value to indicate that
    fact, and userspace can retry as desired.  In addition to stopping
    the scheduler tick, the code takes any actions that might avoid
    a future interrupt to the core, such as a worker thread being
    scheduled that could be quiesced now (e.g. the vmstat worker)
    or a future IPI to the core to clean up some state that could be
    cleaned up now (e.g. the mm lru per-cpu cache).
    
    Once the task has returned to userspace after issuing the prctl(),
    if it enters the kernel again via system call, page fault, or any
    other exception or irq, the kernel will kill it with SIGKILL.
    In addition to sending a signal, the code supports a kernel
    command-line "task_isolation_debug" flag which causes a stack
    backtrace to be generated whenever a task loses isolation.
    
    To allow the state to be entered and exited, the syscall checking
    test ignores the prctl(PR_TASK_ISOLATION) syscall so that we can
    clear the bit again later, and ignores exit/exit_group to allow
    exiting the task without a pointless signal being delivered.
    
    The prctl() API allows for specifying a signal number to use instead
    of the default SIGKILL, to allow for catching the notification
    signal; for example, in a production environment, it might be
    helpful to log information to the application logging mechanism
    before exiting.  Or, the signal handler might choose to reset the
    program counter back to the code segment intended to be run isolated
    via prctl() to continue execution.
    
    In a number of cases we can tell on a remote cpu that we are
    going to be interrupting the cpu, e.g. via an IPI or a TLB flush.
    In that case we generate the diagnostic (and optional stack dump)
    on the remote core to be able to deliver better diagnostics.
    If the interrupt is not something caught by Linux (e.g. a
    hypervisor interrupt) we can also request a reschedule IPI to
    be sent to the remote core so it can be sure to generate a
    signal to notify the process.
    
    Separate patches that follow provide these changes for x86, tile,
    arm, and arm64.
    
    Signed-off-by: Chris Metcalf <cmetcalf@mellanox.com>

--- include/uapi/linux/prctl.h
+++ include/uapi/linux/prctl.h
@@ -197,4 +197,10 @@ struct prctl_mm_map {
 # define PR_CAP_AMBIENT_LOWER		3
 # define PR_CAP_AMBIENT_CLEAR_ALL	4
 
+/* Enable task_isolation mode for TASK_ISOLATION kernels. */
+#define PR_TASK_ISOLATION		48
+# define PR_TASK_ISOLATION_ENABLE	(1 << 0)
+# define PR_TASK_ISOLATION_SET_SIG(sig)	(((sig) & 0x7f) << 8)
+# define PR_TASK_ISOLATION_GET_SIG(bits) (((bits) >> 8) & 0x7f)
+
 #endif /* _LINUX_PRCTL_H */
